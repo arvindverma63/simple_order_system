0a68a902dd3cb2881c8acc7f81d1cc2b1a407cc7
arvindverma63 <arvindverma630635@gmail.com>
2025-06-03 19:34:15 +0530

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -39,7 +39,7 @@
                 <!-- Submit -->
                 <button type="submit"
                     class="w-full bg-blue-600 text-white py-3 rounded-lg font-bold text-lg hover:bg-blue-700 transition duration-300">
-                    Login or Sign Up
+                    Login
                 </button>
             </div>
         </form>
