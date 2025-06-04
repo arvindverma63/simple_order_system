7db40f4ffb4485aac08fdd2c9a0d450da6813370
arvindverma63 <arvindverma630635@gmail.com>
2025-06-04 12:16:18 +0530

    add admin navbar route

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,9 +1,12 @@
     <!-- Navigation Bar -->
     <nav class="bg-white/95 backdrop-blur-lg p-4 rounded-2xl shadow-2xl max-w-4xl mx-auto mb-4">
         <div class="flex justify-center space-x-4">
-            <a href="/"
-                class="text-lg font-bold text-blue-600 bg-blue-100 px-4 py-2 rounded-lg hover:bg-blue-200 transition duration-300">Order
-                Shirts</a>
+            @if (Auth::user()->is_admin === 0)
+                <a href="/"
+                    class="text-lg font-bold text-blue-600 bg-blue-100 px-4 py-2 rounded-lg hover:bg-blue-200 transition duration-300">Order
+                    Shirts</a>
+            @endif
+
             <a href="/orders"
                 class="text-lg font-bold text-blue-600 bg-blue-100 px-4 py-2 rounded-lg hover:bg-blue-200 transition duration-300">See
                 Orders</a>
